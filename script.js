c4d0733995393d34d497b2ba0858d635dec07c2a
xuvi7 <07xuvi@gmail.com>
2024-07-24 14:18:01 -0400

    Implement display controlling
    Results, scoring, restarting
    
    TODO: player names, changing colors, winning sequence highlighting

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const Gameboard = (function() {
-    let board = new Array(9);
-    let moves = 0;
+    let board;
+    let moves;
     const playMove = function(player, slot) {
         if (slot > 8 || slot < 0 || board[slot] !== undefined) {
             return false;
@@ -71,42 +71,124 @@ const Gameboard = (function() {
 
         return res;
     }
-    return {playMove, checkEnd};
+
+    function restart() {
+        board = new Array(9);
+        moves = 0;
+    }
+    return {playMove, checkEnd, restart};
 })();
 
 const createPlayer = (function(mark, name) {
+    let score = 0;
     function getName() {
         return name;
     }
     function playMove(slot) {
         return Gameboard.playMove(mark, slot);
     }
-    return {mark, name, getName, playMove};
+    return {mark, name, score, getName, playMove};
 })
 
 const GameController = (function() {
-    const player1 = createPlayer("X", "Tom");
-    const player2 = createPlayer("O", "Jerry");
-
+    let player1;
+    let player2;
     let currentPlayer;
-    let winningCoords = null;
-
-    const start = prompt("start game");
-
-    while (winningCoords === null) {
+    function startGame(name1, name2) {
+        Gameboard.restart();
+        player1 = createPlayer("X", name1);
+        player2 = createPlayer("O", name2);
+    }
+    
+    function restart() {
+        Gameboard.restart();
+    }
+    
+    function playMove(move) {
         currentPlayer = currentPlayer === player1 ? player2 : player1;
-
-        let move = prompt("input move");
-
+    
         currentPlayer.playMove(move);
 
-        winningCoords = Gameboard.checkEnd(move, currentPlayer.mark);
+        const winningCoords = Gameboard.checkEnd(move, currentPlayer.mark);
+
+        if (winningCoords === null) {
+            return;
+        } else if (winningCoords.length === 0) {
+            endGame(0);
+        } else {
+            let state = 1;
+            if (currentPlayer === player2) {
+                state = 2;
+            }
+            endGame(state);
+        }
     }
 
-    if (winningCoords.length === 0) {
-        console.log("It's a tie");
-    } else {
-        console.log(`${currentPlayer.getName()} wins!`);
+    function endGame(state) {
+        if (state !== 0) {
+            currentPlayer.score++;
+        }
+        DisplayController.endGame(state, currentPlayer);
     }
+
+    function getCurrentPlayer() {
+        return currentPlayer;
+    }
+    
+    return {startGame, playMove, getCurrentPlayer, restart};
 })();
 
+GameController.startGame("Player 1", "Player 2");
+
+const DisplayController = (function() {
+    const board = document.querySelector(".board");
+    const score1 = document.querySelector(".score1");
+    const score2 = document.querySelector(".score2");
+    const restartButton = document.querySelector(".restart");  
+    const results = document.querySelector(".results");  
+
+    restartButton.addEventListener("click", restart);
+    board.addEventListener("click", clickSquare);
+
+    results.addEventListener("click", () => {
+        results.close();
+    })
+
+    function clickSquare(e) {
+        const square = e.target;
+        if (square.classList.contains("X") || square.classList.contains("O")) {
+            return;
+        }
+        GameController.playMove(e.target.id);
+        square.classList.add(GameController.getCurrentPlayer().mark);
+    }
+
+    function restart() {
+        for (const square of board.children) {
+            square.className = "square";
+        }
+        board.addEventListener("click", clickSquare);
+        GameController.restart();
+    }
+
+    function endGame(state, winner) {
+        board.removeEventListener("click", clickSquare);
+        if (state === 1) {
+            score1.textContent = winner.score;
+        } else if (state === 2) {
+            score2.textContent = winner.score;
+        }
+        displayResults(state, winner);
+    }
+
+    function displayResults(state, winner) {
+        if (state === 0) {
+            results.textContent = "It's a tie!";
+        } else {
+            results.textContent = `${winner.name} wins!`;
+        }
+        results.showModal();
+    }
+
+    return {endGame};
+})();
